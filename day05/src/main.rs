205f1a3b6822f65c2526068cb2258c5f5724e41b
Robert Buchberger <robert@buchberger.cc>
2023-12-09 13:16:36 +0100

    Day 5 part 2 WIP
    It runs, and it correctly solves the example problem, but it doesn't
    solve the actual problem. There's a logic error somewhere, probably to
    do with the boundadry checking, but between watching a sick kid all week
    and my actual job I don't have time to actually find it. Sorry :(

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,8 +1,12 @@
 use std::collections::HashMap;
+use std::ops::RangeInclusive;
 
 fn main() {
     println!("Part one example: {}", part_one("example"));
     println!("Part one actual:  {}", part_one("input"));
+
+    println!("Part two example: {}", part_two("example"));
+    println!("Part two actual:  {}", part_two("input"));
 }
 
 fn part_one(filename: &str) -> usize {
@@ -10,19 +14,100 @@ fn part_one(filename: &str) -> usize {
 
     return seeds
         .iter()
-        .map(|s| trace_down(*s, "seed", &maps).1)
+        .map(|s| {
+            trace_down(
+                Trace {
+                    key: String::from("seed"),
+                    value: *s,
+                    bound_dist: None,
+                },
+                &maps,
+            )
+            .value
+        })
         .min()
         .unwrap();
 }
 
-fn trace_down<'a>(
+fn part_two(filename: &str) -> usize {
+    let (seeds, maps) = parse_file(filename);
+    let seeds = get_seed_ranges(seeds);
+
+    let result = seeds
+        .iter()
+        .flat_map(|s| trace_range(s, &maps))
+        .min_by_key(|s| s.value)
+        .unwrap()
+        .value;
+
+    return result;
+}
+
+fn trace_range(range: &RangeInclusive<usize>, maps: &HashMap<String, CategoryMap>) -> Vec<Trace> {
+    let mut values = Vec::new();
+    let mut value = range.start().clone();
+
+    let trace = Trace {
+        key: String::from("seed"),
+        value,
+        bound_dist: None,
+    };
+
+    while range.contains(&value) {
+        let trace = trace_down(trace.clone(), maps);
+
+        value = match trace.bound_dist {
+            Some(bound_dist) => value + bound_dist,
+            None => *range.end(),
+        };
+
+        values.push(trace);
+    }
+
+    return values;
+}
+
+fn get_seed_ranges(seeds: Vec<usize>) -> Vec<RangeInclusive<usize>> {
+    seeds
+        .windows(2)
+        .step_by(2)
+        .map(|slice| {
+            let start = slice[0];
+            let end = start + slice[1];
+
+            return start..=end;
+        })
+        .collect()
+}
+
+fn trace_down(trace: Trace, maps: &HashMap<String, CategoryMap>) -> Trace {
+    match maps.get(&trace.key) {
+        None => trace,
+        Some(m) => trace_down(
+            Trace {
+                key: m.output_key.clone(),
+                value: m.translate(trace.value),
+                bound_dist: Trace::compare_bounds(trace.bound_dist, m.bound_dist(trace.value)),
+            },
+            maps,
+        ),
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Trace {
+    key: String,
     value: usize,
-    key: &'a str,
-    maps: &'a HashMap<String, CategoryMap>,
-) -> (&'a str, usize) {
-    match maps.get(key) {
-        None => (key, value),
-        Some(m) => trace_down(m.translate(value), &m.output_key, maps),
+    bound_dist: Option<usize>,
+}
+
+impl Trace {
+    fn compare_bounds(a: Option<usize>, b: Option<usize>) -> Option<usize> {
+        let bounds = vec![a, b];
+        let mut bounds = bounds.iter().filter_map(|v| *v).collect::<Vec<usize>>();
+        bounds.sort();
+
+        return bounds.first().copied();
     }
 }
 
@@ -31,6 +116,7 @@ struct CategoryMap {
     input_key: String,
     output_key: String,
     ranges: Vec<RangeConversion>,
+    boundaries: Vec<usize>,
 }
 
 impl CategoryMap {
@@ -39,6 +125,12 @@ impl CategoryMap {
             None => input,
             Some(v) => v,
         }
+    }
+
+    fn bound_dist(&self, input: usize) -> Option<usize> {
+        let bound = self.boundaries.iter().find(|v| **v > input)?;
+
+        return Some(bound - input);
     }
 }
 
@@ -95,11 +187,18 @@ impl From<&str> for CategoryMap {
         let output_key = keys.next().unwrap().into();
 
         let ranges: Vec<_> = lines.map(RangeConversion::from).collect();
+        let mut boundaries: Vec<_> = ranges
+            .iter()
+            .flat_map(|r| vec![r.source, r.source + r.length])
+            .collect();
+
+        boundaries.sort();
 
         return Self {
             input_key,
             output_key,
             ranges,
+            boundaries,
         };
     }
 }
